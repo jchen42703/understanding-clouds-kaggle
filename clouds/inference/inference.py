1bcfc999959f29c5da61fcc2ff7ad67201919016
Joseph <jchen42703@gmail.com>
2019-09-12 06:22:29 -0400

    First go at stratification
    Not really a big commit, still need to get it to working state. Just committing to reduce conflicts.

--- clouds/inference/inference.py
+++ clouds/inference/inference.py
@@ -1,5 +1,4 @@
 import tqdm
-
 import gc
 
 torch.cuda.empty_cache()
@@ -10,17 +9,44 @@ test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=
 
 loaders = {"test": test_loader}
 
-encoded_pixels = []
-for i, test_batch in enumerate(tqdm.tqdm(loaders['test'])):
-    runner_out = runner.predict_batch({"features": test_batch[0].cuda()})['logits']
-    for i, batch in enumerate(runner_out):
-        for probability in batch:
-            probability = probability.cpu().detach().numpy()
+def get_probabilities():
+    encoded_pixels = []
+    loaders = {"infer": valid_loader}
+    runner.infer(
+        model=model,
+        loaders=loaders,
+        callbacks=[
+            CheckpointCallback(
+                resume=f"{logdir}/checkpoints/best.pth"),
+            InferCallback()
+        ],
+    )
+    valid_masks = []
+    probabilities = np.zeros((2220, 350, 525))
+    for i, (batch, output) in enumerate(tqdm.tqdm(zip(
+            valid_dataset, runner.callbacks[0].predictions["logits"]))):
+        image, mask = batch
+        for m in mask:
+            if m.shape != (350, 525):
+                m = cv2.resize(m, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+            valid_masks.append(m)
+
+        for j, probability in enumerate(output):
             if probability.shape != (350, 525):
                 probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
-            predict, num_predict = post_process(sigmoid(probability), best_threshold, best_size)
-            if num_predict == 0:
-                encoded_pixels.append('')
-            else:
-                r = mask2rle(predict)
-                encoded_pixels.append(r)
+            probabilities[i * 4 + j, :, :] = probability
+def create_submission():
+    encoded_pixels = []
+    for i, test_batch in enumerate(tqdm.tqdm(loaders['test'])):
+        runner_out = runner.predict_batch({"features": test_batch[0].cuda()})['logits']
+        for i, batch in enumerate(runner_out):
+            for probability in batch:
+                probability = probability.cpu().detach().numpy()
+                if probability.shape != (350, 525):
+                    probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+                predict, num_predict = post_process(sigmoid(probability), best_threshold, best_size)
+                if num_predict == 0:
+                    encoded_pixels.append('')
+                else:
+                    r = mask2rle(predict)
+                    encoded_pixels.append(r)
