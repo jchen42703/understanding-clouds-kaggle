6ea5049f2e695c6528cf39c32c4908b29eed2158
jchen42703 <jchen42703@gmail.com>
2019-10-26 11:08:07 -0400

    Changing version to support breaking dataset changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='clouds',
-      version='0.01.1',
+      version='0.01.2',
       description='Understanding Clouds from Satellite Images',
       url='',
       author='Joseph Chen',
