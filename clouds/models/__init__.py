4a2a766ee8f57eac085726c4cb3d3b5cc2169b06
jchen42703 <jchen42703@gmail.com>
2019-11-16 17:54:43 -0500

    Fixed up __init__.py files

--- clouds/models/__init__.py
+++ clouds/models/__init__.py
@@ -1,2 +1,2 @@
 from .clf_models import Pretrained
-from .unet_models import Res34Unetv3, Res34Unetv4, Res34Unetv5
+from .seg import Res34Unetv3, Res34Unetv4, Res34Unetv5, ResNet34FPN
---
+++ clouds/models/seg/__init__.py
@@ -0,0 +1,2 @@
+from .fpn_resnet34 import ResNet34FPN
+from .unet_models import Res34Unetv3, Res34Unetv4, Res34Unetv5
