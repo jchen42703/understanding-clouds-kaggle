dc58d75ab79a61a8036e3e9dbdca73b0973b9dbf
jchen42703 <jchen42703@gmail.com>
2019-11-16 17:54:32 -0500

    Create fpn_resnet34.py
    FPN with a ResNet34 encoder (does clf + seg)

---
+++ clouds/models/seg/fpn_resnet34.py
@@ -0,0 +1,73 @@
+import torchvision
+import torch.nn as nn
+
+from .model_utils import SCse, ConvGnUp2d
+
+class ResNet34FPN(nn.Module):
+    def __init__(self, num_class=4):
+        super(Net, self).__init__()
+
+        # e = ResNet34()
+        self.resnet = torchvision.models.resnet34(True)
+        self.conv1 = nn.Sequential(
+            self.resnet.conv1,
+            self.resnet.bn1,
+            self.resnet.relu)
+
+        self.encode2 = nn.Sequential(self.resnet.layer1,
+                                     SCse(64))
+        self.encode3 = nn.Sequential(self.resnet.layer2,
+                                     SCse(128))
+        self.encode4 = nn.Sequential(self.resnet.layer3,
+                                     SCse(256))
+        self.encode5 = nn.Sequential(self.resnet.layer4,
+                                     SCse(512))
+        #---
+        self.lateral0 = nn.Conv2d(512, 128, kernel_size=1, padding=0, stride=1)
+        self.lateral1 = nn.Conv2d(256, 128, kernel_size=1, padding=0, stride=1)
+        self.lateral2 = nn.Conv2d(128, 128, kernel_size=1, padding=0, stride=1)
+        self.lateral3 = nn.Conv2d(64, 128, kernel_size=1, padding=0, stride=1)
+
+        self.top1 = nn.Sequential(
+            ConvGnUp2d(128,128, True),
+            ConvGnUp2d(128, 64, True),
+            ConvGnUp2d( 64, 64, True),
+        )
+        self.top2 = nn.Sequential(
+            ConvGnUp2d(128, 64, True),
+            ConvGnUp2d(64, 64, True),
+        )
+        self.top3 = nn.Sequential(
+            ConvGnUp2d(128, 64, True),
+        )
+        self.logit = nn.Conv2d(64*3,num_class,kernel_size=1)
+
+
+    def forward(self, x):
+        batch_size,C,H,W = x.shape
+
+        x0 = self.conv1(x)
+        x1 = self.encode2(x0)
+        x2 = self.encode3(x1)
+        x3 = self.encode4(x2)
+        x4 = self.encode5(x3)
+
+        ##----
+        #segment
+
+        t0 = self.lateral0(x4)
+        t1 = upsize_add(t0, self.lateral1(x3)) #16x16
+        t2 = upsize_add(t1, self.lateral2(x2)) #32x32
+        t3 = upsize_add(t2, self.lateral3(x1)) #64x64
+
+        t1 = self.top1(t1) #; print(t1.shape)
+        t2 = self.top2(t2) #; print(t2.shape)
+        t3 = self.top3(t3) #; print(t3.shape)
+
+        x = fuse([t1,t2,t3], 'cat')
+        logit = self.logit(x)
+
+        #---
+        probability_mask = torch.sigmoid(logit)
+        probability_label = F.adaptive_max_pool2d(probability_mask, 1).view(batch_size,-1)
+        return (probability_label, probability_mask)
