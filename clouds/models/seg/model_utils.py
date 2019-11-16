513e47d903675a09ae0c7845632cdabcff3badde
jchen42703 <jchen42703@gmail.com>
2019-11-16 17:54:13 -0500

    Split unet_models.py into seg/unet_models.py and seg/model_utils.py
    model_utils.py also contains some utilities for the upcoming FPN commit

---
+++ clouds/models/seg/model_utils.py
@@ -0,0 +1,81 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SpatialAttention2d(nn.Module):
+    def __init__(self, channel):
+        super(SpatialAttention2d, self).__init__()
+        self.squeeze = nn.Conv2d(channel, 1, kernel_size=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        z = self.squeeze(x)
+        z = self.sigmoid(z)
+        return x * z
+
+class GAB(nn.Module):
+    def __init__(self, input_dim, reduction=4):
+        super(GAB, self).__init__()
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
+        self.conv1 = nn.Conv2d(input_dim, input_dim // reduction, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(input_dim // reduction, input_dim, kernel_size=1, stride=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        z = self.global_avgpool(x)
+        z = self.relu(self.conv1(z))
+        z = self.sigmoid(self.conv2(z))
+        return x * z
+
+class SCse(nn.Module):
+    def __init__(self, dim):
+        super(SCse, self).__init__()
+        self.satt = SpatialAttention2d(dim)
+        self.catt = GAB(dim)
+
+    def forward(self, x):
+        return self.satt(x) + self.catt(x)
+
+def resize_like(x, reference, mode="bilinear"):
+    if x.shape[2:] !=  reference.shape[2:]:
+        if mode=="bilinear":
+            x = F.interpolate(x, size=reference.shape[2:],mode="bilinear",
+                              align_corners=False)
+        if mode=="nearest":
+            x = F.interpolate(x, size=reference.shape[2:], mode="nearest")
+    return x
+
+
+def upsize_add(x, lateral):
+    x = resize_like(x, lateral, mode="nearest")  + lateral
+    return x
+
+
+def fuse(x, mode="cat"):
+    batch_size, C0, H0, W0 = x[0].shape
+
+    for i in range(1,len(x)):
+        _,_,H,W = x[i].shape
+        if (H, W)!=(H0, W0):
+            x[i] = F.interpolate(x[i], size=(H0,W0), mode="bilinear",
+                                 align_corners=False)
+    if mode=="cat":
+        return torch.cat(x, 1)
+
+
+class ConvGnUp2d(nn.Module):
+    def __init__(self, in_channel, out_channel, is_upsize=None, num_group=32,
+                 kernel_size=3, padding=1, stride=1):
+        super(ConvGnUp2d, self).__init__()
+        self.is_upsize = is_upsize
+        self.conv = nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size,
+                              padding=padding, stride=stride, bias=False)
+        self.gn   = nn.GroupNorm(num_group,out_channel)
+
+    def forward(self,x):
+        x = self.conv(x)
+        x = self.gn(x)
+        x = F.relu(x, inplace=True)
+        if self.is_upsize :
+            x = F.interpolate(x, scale_factor=2, mode="bilinear",align_corners=False)
+        return x
--- clouds/models/seg/unet_models.py
+++ clouds/models/seg/unet_models.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
+from .model_utils import SpatialAttention2d, GAB, SCse
+
 class FPAv2(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(FPAv2, self).__init__()
@@ -56,35 +58,6 @@ def conv3x3(input_dim, output_dim, rate=1):
                          nn.BatchNorm2d(output_dim),
                          nn.ELU(True))
 
-
-class SpatialAttention2d(nn.Module):
-    def __init__(self, channel):
-        super(SpatialAttention2d, self).__init__()
-        self.squeeze = nn.Conv2d(channel, 1, kernel_size=1, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        z = self.squeeze(x)
-        z = self.sigmoid(z)
-        return x * z
-
-
-class GAB(nn.Module):
-    def __init__(self, input_dim, reduction=4):
-        super(GAB, self).__init__()
-        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
-        self.conv1 = nn.Conv2d(input_dim, input_dim // reduction, kernel_size=1, stride=1)
-        self.conv2 = nn.Conv2d(input_dim // reduction, input_dim, kernel_size=1, stride=1)
-        self.relu = nn.ReLU(inplace=True)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        z = self.global_avgpool(x)
-        z = self.relu(self.conv1(z))
-        z = self.sigmoid(self.conv2(z))
-        return x * z
-
-
 class Decoder(nn.Module):
     def __init__(self, in_channels, channels, out_channels):
         super(Decoder, self).__init__()
@@ -125,17 +98,6 @@ class Decoderv2(nn.Module):
         s = self.s_att(cat_p)
         c = self.c_att(cat_p)
         return s + c
-
-
-class SCse(nn.Module):
-    def __init__(self, dim):
-        super(SCse, self).__init__()
-        self.satt = SpatialAttention2d(dim)
-        self.catt = GAB(dim)
-
-    def forward(self, x):
-        return self.satt(x) + self.catt(x)
-
 
 # stage1 model
 class Res34Unetv4(nn.Module):
