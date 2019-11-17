28229bf96e89316f857498625a1c9f812e071c36
jchen42703 <jchen42703@gmail.com>
2019-11-16 19:34:09 -0500

    Added aug6

--- scripts/utils.py
+++ scripts/utils.py
@@ -90,18 +90,35 @@ def get_training_augmentation(augmentation_key="aug5"):
                                 albu.GridDistortion(p=0.5),
                                 albu.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5),
                               ],
+                      "aug6": [
+                                albu.HorizontalFlip(p=0.5),
+                                albu.VerticalFlip(p=0.5),
+                                albu.ShiftScaleRotate(scale_limit=0.1, rotate_limit=0,
+                                                      shift_limit=0.5, p=0.5,
+                                                      border_mode=0),
+                                albu.OneOf([
+                                            albu.RandomResizedCrop(height=696, width=1048,
+                                                                   scale=(1.0, 0.9),
+                                                                   ratio=(0.75, 1.33)),
+                                            albu.RandomCrop(height=696, width=1048)
+                                           ], p=0.3),
+                                albu.Lambda(image=do_noise, p=0.5),
+                              ],
                      }
     train_transform = transform_dict[augmentation_key]
     return albu.Compose(train_transform)
 
-def get_validation_augmentation(augmentation_key):
-    """Add paddings to make image shape divisible by 32"""
+def get_validation_augmentation(augmentation_key="aug5"):
+    """
+    Validation transforms
+    """
     transform_dict = {
                       "aug1": [],
                       "aug2": [],
                       "aug3": [albu.RandomCrop(height=320, width=640, p=1)],
                       "aug4": [albu.RandomCrop(height=320, width=320, p=1)],
                       "aug5": [albu.RandomCrop(height=320, width=320, p=1)],
+                      "aug6": [albu.RandomCrop(height=696, width=1048, p=1)],
                      }
     test_transform = transform_dict[augmentation_key]
     return albu.Compose(test_transform)
